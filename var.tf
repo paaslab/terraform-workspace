f3b1cca00de9ad3db9a5575a60761c34db395d3d
Xi Chen <superxi@aliyun.com>
2020-02-27 20:23:20 +0800

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,3 +1,15 @@
 variable "nginx_version" {
   default = "1.7.8"
 }
+
+variable "client-crt" {
+  type = string
+}
+
+variable "client-key" {
+  type = string
+}
+
+variable "ca-crt" {
+  dtype = string
+}
